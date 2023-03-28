3c058c614b4402bf7adf11e502c3caa4196906c8
Fraser Adams <fraser.adams@blueyonder.co.uk>
2023-03-28 14:36:16 +0100

    Update image-greyscale to use image crate v0.24.6, which also required some code changes.

--- examples/wasm/image-greyscale/image-greyscale-rust/src/main.rs
+++ examples/wasm/image-greyscale/image-greyscale-rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Cursor, Read, Write};
 use image::{ImageOutputFormat, ImageFormat};
 
 fn main() {
@@ -8,7 +8,10 @@ fn main() {
     let image_format_detected: ImageFormat = image::guess_format(&buf).unwrap();
     let img = image::load_from_memory(&buf).unwrap();
     let filtered = img.grayscale();
-    let mut buf = vec![];
+    // In image 0.24.6 DynamicImage write_to requires the Seek trait which
+    // wasn't required in image 0.23.0 used in the original version of this
+    // example, so we now use Cursor::new(vec![]) instead of just vec![]
+    let mut buf = Cursor::new(vec![]);
     match image_format_detected {
         ImageFormat::Gif => {
             filtered.write_to(&mut buf, ImageOutputFormat::Gif).unwrap();
@@ -17,6 +20,8 @@ fn main() {
             filtered.write_to(&mut buf, ImageOutputFormat::Png).unwrap();
         },
     };
-    io::stdout().write_all(&buf).unwrap();
+    // into_inner consumes the cursor, returning the underlying value.
+    // https://doc.rust-lang.org/std/io/struct.Cursor.html#method.into_inner
+    io::stdout().write_all(&buf.into_inner()).unwrap();
     io::stdout().flush().unwrap();
 }
